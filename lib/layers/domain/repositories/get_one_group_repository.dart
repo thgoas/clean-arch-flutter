0187e4857c0f2347c76fab9de9ddfa113af959b0
Thiago Andrade <thgas95@gmail.com>
2024-06-01 12:01:21 -0300

    feat: create get one group repository

---
+++ lib/layers/domain/repositories/get_one_group_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:fin_ca/layers/domain/entities/group.dart';
+import 'package:fin_ca/layers/domain/erros/failure_get_group.dart';
+
+abstract class GetOneGroupRepository {
+  Future<Either<FailureGetGroup, Group>> getOneGroup(String id);
+}
