ba50c59f5a4a1976c83d5fea360f5a85da788fa7
Thiago Andrade <thgas95@gmail.com>
2024-06-03 15:06:02 -0300

    test: add test usecase

--- test/layers/domain/usecases/get_one_group_use_case_test.dart
+++ test/layers/domain/usecases/get_one_group_use_case_test.dart
@@ -1,23 +1,41 @@
 import 'package:dartz/dartz.dart';
 import 'package:fin_ca/layers/domain/entities/group.dart';
+import 'package:fin_ca/layers/domain/erros/failure_get_group.dart';
 import 'package:fin_ca/layers/domain/repositories/get_one_group_repository.dart';
 import 'package:fin_ca/layers/domain/usecases/get_one_group_use_case_imp.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
 class GetOneRepositoryMock extends Mock implements GetOneGroupRepository {}
 
 void main() {
-  final repository = GetOneRepositoryMock();
-  final usecase = GetOneGroupUseCaseImp(repository);
-  test('should return one group', () async {
-    when(repository.getOneGroup('1')).thenAnswer(
-      (_) async => Right(
-          {"id": "1", "description": "description", "type": "type"} as Group),
+  late GetOneRepositoryMock repository;
+  late GetOneGroupUseCaseImp usecase;
+  final testGroup = Group(id: '1', description: 'desc', type: 'type');
+  setUpAll(() {
+    // Registre o comportamento esperado para argumentos não nulos
+    registerFallbackValue('');
+  });
+  setUp(() {
+    repository = GetOneRepositoryMock();
+    usecase = GetOneGroupUseCaseImp(repository);
+  });
+  test('should return a group when the id is valid', () async {
+    when(() => repository.getOneGroup('1')).thenAnswer(
+      (_) async => Right(testGroup),
     );
     final result = await usecase("1");
 
     expect(result, isA<Right>());
-    // expect(result, isA<Group>());
+    expect(result, Right(testGroup));
+  });
+
+  test('should return a failureGroup when there is a failure', () async {
+    when(() => repository.getOneGroup(any())).thenAnswer(
+      (_) async => Left(InvalidIdError()),
+    );
+    final result = await usecase("1");
+    expect(result, isA<Left>());
+    expect(result.fold(id, id), isA<InvalidIdError>());
   });
 }
